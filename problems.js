22e6d2e16c0a77af98e341ebfd56afbc142b4cf2
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-12-12 22:22:21 +0600

    frist problem done

---
+++ problems.js
@@ -0,0 +1,25 @@
+// 01 -- count the character
+// time complexity Big O(n) or linear
+// space complexity O(n)
+function countCharacter(string) {
+  // creating a new object
+  const hashMap = {};
+  // normalized the object
+  const normalized = string.toLowerCase();
+  // looping the object
+  for (let i = 0; i < normalized.length; i++) {
+    const char = normalized[i];
+    // ignoring the space
+    if (char === " ") continue;
+    // if the character is existing in object
+    if (char in hashMap) {
+      hashMap[char] = hashMap[char] + 1;
+    } else {
+      hashMap[char] = 1;
+    }
+  }
+  return hashMap;
+}
+
+const letter = countCharacter("Hello world");
+console.log(letter);
