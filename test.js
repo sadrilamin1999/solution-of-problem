8971b267197675fc36a00be81354078006c62592
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-12-13 00:37:32 +0600

    problem 2d is updated

--- test.js
+++ test.js
@@ -1,6 +1,18 @@
 // is element exists
-
-function isElementExists(arr1, arr2) {
-  console.log(arr1, arr2);
+// time complexity Big O(n*n)
+// space complexity O(1) - because output dose not depends on input
+function isExists(arr1, arr2) {
+  // initial state to return a single result out of loop
+  let state = false;
+  // loop the 1st array
+  for (elm of arr1) {
+    // looping 2nd array & check if element of 1st array is included in 2nd array
+    if (arr2.includes(elm)) {
+      state = true;
+      // if the condition is true no need to check next condition
+      break;
+    } else state = false;
+  }
+  return state;
 }
-isElementExists(["a", "b", "c"], [1, 2, 3]);
+console.log(isExists(["a", "b", "c"], [1, "b", 3]));
